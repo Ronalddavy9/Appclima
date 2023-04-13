45fbde77ce3b7d35797b98f83e703b0cdb3de994
Ronald Davy <ronalddavy9@gmail.com>
2023-04-13 15:06:32 -0300

    Update app.js
    alterando logica de requisição

--- app.js
+++ app.js
@@ -18,12 +18,14 @@ const sunsetTime = document.getElementById("sunset-time");
 const api_key = "896ed49c5ba70ed7875c3f89e7ef0128";
 
 
-citySearchButton.addEventListener("click", () => {
-
-   let cityName = citySearchInput.value
-   getCityWeather(cityName)
-
-})
+citySearchButton.addEventListener('click', () => {
+  const cityNameInput = citySearchInput.value;
+  if (cityNameInput) {
+    getCityWeather(cityNameInput);
+  } else {
+    alert('Por favor, insira o nome da cidade!');
+  }
+});
 
 navigator.geolocation.getCurrentPosition((position) => {
   console.log(position);
@@ -31,10 +33,20 @@ navigator.geolocation.getCurrentPosition((position) => {
 
 
 function getCityWeather(cityName) {
-   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
-     .then((response) => response.json())
-     .then((data) => displayWeather(data))
-
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Não foi possível obter os dados do clima. Por favor, tente novamente!');
+      }
+      return response.json();
+    })
+    .then((data) => displayWeather(data))
+    .catch((error) => {
+      console.error(error);
+      alert(error.message);
+    });
 }
 
 function displayWeather(data) {
